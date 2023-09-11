7ebc341e5332b0133e639c871e2c535686ba88ea
Kelvin Brito <britok30@gmail.com>
2023-09-11 17:51:26 -0400

    navbar complete

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Avatar = () => {
   return (
     <Image
-      className="rounded-full"
+      className="rounded-full select-none"
       height="30"
       width="30"
       alt="avatar"
